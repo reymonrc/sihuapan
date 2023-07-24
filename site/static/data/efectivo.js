4bc0238fa57cb65e08b78a2b463ab7dea1335a57
ReyMon <ramon@reymon.mx>
2023-07-24 11:05:49 -0600

    Update efectivo.js

--- site/static/data/efectivo.js
+++ site/static/data/efectivo.js
@@ -30,4 +30,5 @@ var datos=[{ "fecha": "2023-05-06", "entrada": "0", "salida": "1000", "concepto"
 { "fecha": "2023-06-10", "entrada": "0", "salida": "350", "concepto": "Día Sr Sergio", "efectivo": true },
 { "fecha": "2023-06-25", "entrada": "0", "salida": "1000", "concepto": "Prestamo Ramón", "efectivo": false },
 { "fecha": "2023-07-17", "entrada": "5500", "salida": "0", "concepto": "Transferencia a Yair del burro", "efectivo": false },
+{ "fecha": "2023-07-22", "entrada": "9900", "salida": "0", "concepto": "Retiro cajero RRC", "efectivo": false },
            ]
