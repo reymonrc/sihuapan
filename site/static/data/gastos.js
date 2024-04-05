8ac3d6660cfb962f05cd8a21e150b9d7c14c8d92
ReyMon <ramon@reymon.mx>
2024-04-05 19:48:03 +0000

    ctas 2024 3

--- site/static/data/gastos.js
+++ site/static/data/gastos.js
@@ -1,22 +1,22 @@
 var datos22=[
-{"fecha": "Jun", "salida": "0", "entrada": "50000", "concepto": "Venta mango", "efectivo": true},
+{"fecha": "Jun", "salida": "0", "entrada": "48000", "concepto": "Venta mango", "efectivo": true},
 {"fecha": "Jul-Dic", "salida": "0", "entrada": "8000", "concepto": "Renta soltadero", "efectivo": true},
 {"fecha": "Jun", "salida": "0", "entrada": "15000", "concepto": "Renta 1ha tabaco-soltadero", "efectivo": true},
 {"fecha": "Jun", "salida": "0", "entrada": "15000", "concepto": "Renta 1ha tabaco-cocal", "efectivo": true},
 {"fecha": "Sep", "salida": "0", "entrada": "11000", "concepto": "Venta de leña, poda finca", "efectivo": true},
 {"fecha": "Nov", "salida": "0", "entrada": "4000", "concepto": "Renta 2has maiz-soltadero", "efectivo": true},
-{"fecha": "May-Dic", "salida": "75400", "entrada": "0", "concepto": "Mano de obra - campo", "efectivo": true},
+{"fecha": "May-Dic", "salida": "77400", "entrada": "0", "concepto": "Mano de obra - campo", "efectivo": true},
 {"fecha": "Abr-Dic", "salida": "38000", "entrada": "0", "concepto": "Insumos - campo", "efectivo": true},
 ];
 datos23=[
-{"fecha": "Jun", "salida": "0", "entrada": "35000", "concepto": "Venta mango", "efectivo": true},
+{"fecha": "Jun", "salida": "0", "entrada": "32000", "concepto": "Venta mango", "efectivo": true},
 {"fecha": "Ene-Dic", "salida": "0", "entrada": "34000", "concepto": "Renta soltadero", "efectivo": true},
 {"fecha": "Ene-Dic", "salida": "20000", "entrada": "0", "concepto": "Renta soltadero (debe)", "efectivo": true},
 {"fecha": "Jun", "salida": "0", "entrada": "37500", "concepto": "Renta 2.5has tabaco-soltadero", "efectivo": true},
 {"fecha": "Jun", "salida": "0", "entrada": "15000", "concepto": "Renta 1ha tabaco-cocal", "efectivo": true},
 {"fecha": "Nov", "salida": "0", "entrada": "4000", "concepto": "Renta 2has maiz-soltadero", "efectivo": true},
-{"fecha": "Ene-Dic", "salida": "73600", "entrada": "0", "concepto": "Mano de obra - campo", "efectivo": true},
-{"fecha": "Ene-Dic", "salida": "47000", "entrada": "0", "concepto": "Insumos - campo", "efectivo": true},
+{"fecha": "Ene-Dic", "salida": "75600", "entrada": "0", "concepto": "Mano de obra - campo", "efectivo": true},
+{"fecha": "Ene-Dic", "salida": "49000", "entrada": "0", "concepto": "Insumos - campo", "efectivo": true},
 ];
 datos24=[
 {"fecha": "Ene", "salida": "0", "entrada": "1000", "concepto": "Renta 1ha sandia-cocal", "efectivo": true},
