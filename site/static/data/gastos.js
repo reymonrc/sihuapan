8fde8e1dd9f791099d45c7d6f91e9ba1c7effc65
ReyMon <ramon@reymon.mx>
2024-04-05 18:22:12 +0000

    ctas

--- site/static/data/gastos.js
+++ site/static/data/gastos.js
@@ -1,15 +1,15 @@
 var datos22=[
-{"fecha": "Jun", "salida": "", "entrada": "50000", "concepto": "Venta mango"},
-{"fecha": "Ene-Dic", "salida": "", "entrada": "12000", "concepto": "Renta soltadero"},
-{"fecha": "Jun", "salida": "", "entrada": "15000", "concepto": "Renta 1 has tabaco-soltadero"},
-{"fecha": "Jun", "salida": "", "entrada": "15000", "concepto": "Renta 1 has tabaco-cocal"},
-{"fecha": "Nov", "salida": "", "entrada": "4000", "concepto": "Renta 2 has maiz-soltadero"},
+{"fecha": "Jun", "salida": "0", "entrada": "50000", "concepto": "Venta mango", "efectivo": true},
+{"fecha": "Ene-Dic", "salida": "0", "entrada": "12000", "concepto": "Renta soltadero", "efectivo": true},
+{"fecha": "Jun", "salida": "0", "entrada": "15000", "concepto": "Renta 1 has tabaco-soltadero", "efectivo": true},
+{"fecha": "Jun", "salida": "0", "entrada": "15000", "concepto": "Renta 1 has tabaco-cocal", "efectivo": true},
+{"fecha": "Nov", "salida": "0", "entrada": "4000", "concepto": "Renta 2 has maiz-soltadero", "efectivo": true},
 ];
 datos23=[
-{"fecha": "Jun", "salida": "", "entrada": "35000", "concepto": "Venta mango"},
-{"fecha": "Ene-Dic", "salida": "", "entrada": "34000", "concepto": "Renta soltadero"},
-{"fecha": "Ene-Dic", "salida": "20000", "entrada": "", "concepto": "Renta soltadero"},
-{"fecha": "Jun", "salida": "", "entrada": "37500", "concepto": "Renta 2.5 has tabaco-soltadero"},
-{"fecha": "Jun", "salida": "", "entrada": "15000", "concepto": "Renta 1 has tabaco-cocal"},
-{"fecha": "Nov", "salida": "", "entrada": "4000", "concepto": "Renta 2 has maiz-soltadero"},
+{"fecha": "Jun", "salida": "0", "entrada": "35000", "concepto": "Venta mango", "efectivo": true},
+{"fecha": "Ene-Dic", "salida": "0", "entrada": "34000", "concepto": "Renta soltadero", "efectivo": true},
+{"fecha": "Ene-Dic", "salida": "20000", "entrada": "0", "concepto": "Renta soltadero", "efectivo": true},
+{"fecha": "Jun", "salida": "0", "entrada": "37500", "concepto": "Renta 2.5 has tabaco-soltadero", "efectivo": true},
+{"fecha": "Jun", "salida": "0", "entrada": "15000", "concepto": "Renta 1 has tabaco-cocal", "efectivo": true},
+{"fecha": "Nov", "salida": "0", "entrada": "4000", "concepto": "Renta 2 has maiz-soltadero", "efectivo": true},
 ];
