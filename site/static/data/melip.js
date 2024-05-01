d06e2d8fa558eba7a56089e4d90da50bdacc7d98
ReyMon <ramon@reymon.mx>
2024-05-01 00:04:30 +0000

    gastos meliponas

---
+++ site/static/data/melip.js
@@ -0,0 +1,5 @@
+var datosmeli=[
+{"monto": "22500", "concepto": "50 Cajas", "efectivo": true},
+{"monto": "8000", "concepto": "100 Trampas", "efectivo": true},
+{"monto": "9050", "concepto": "Meliponario", "efectivo": true},
+];
