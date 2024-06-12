528d325b5c9545fca57a89a7a00019da0729a9e9
ReyMon <ramon@reymon.mx>
2024-06-12 21:49:19 +0000

    prorrateo aportacion ACRC

--- site/static/data/gastos.js
+++ site/static/data/gastos.js
@@ -7,6 +7,7 @@ var datos22=[
 {"fecha": "Nov", "salida": "0", "entrada": "4000", "concepto": "Renta 2has maiz-soltadero", "efectivo": true},
 {"fecha": "May-Dic", "salida": "77400", "entrada": "0", "concepto": "Mano de obra - campo", "efectivo": true},
 {"fecha": "Abr-Dic", "salida": "38000", "entrada": "0", "concepto": "Insumos - campo", "efectivo": true},
+{"fecha": "Dic", "salida": "0", "entrada": "5000", "concepto": "Aportación Arturo Carrión", "efectivo": false},
 ];
 datos23=[
 {"fecha": "Jun", "salida": "0", "entrada": "32000", "concepto": "Venta mango", "efectivo": true},
@@ -17,11 +18,12 @@ datos23=[
 {"fecha": "Nov", "salida": "0", "entrada": "4000", "concepto": "Renta 2has maiz-soltadero", "efectivo": true},
 {"fecha": "Ene-Dic", "salida": "75600", "entrada": "0", "concepto": "Mano de obra - campo", "efectivo": true},
 {"fecha": "Ene-Dic", "salida": "49000", "entrada": "0", "concepto": "Insumos - campo", "efectivo": true},
+{"fecha": "Dic", "salida": "0", "entrada": "5000", "concepto": "Aportación Arturo Carrión", "efectivo": false},
 ];
 datos24=[
 {"fecha": "Ene", "salida": "0", "entrada": "1000", "concepto": "Renta 1ha sandia-cocal", "efectivo": true},
 {"fecha": "Mar-Jun", "salida": "0", "entrada": "10000", "concepto": "Renta soltadero", "efectivo": true},
-{"fecha": "Abr", "salida": "0", "entrada": "15000", "concepto": "Aportación Arturo Carrión", "efectivo": false},
+{"fecha": "Abr", "salida": "0", "entrada": "5000", "concepto": "Aportación Arturo Carrión", "efectivo": false},
 {"fecha": "Abr", "salida": "11940", "entrada": "0", "concepto": "Casita: block, cemento, cal", "efectivo": false},
 {"fecha": "Abr 27", "salida": "0", "entrada": "15330", "concepto": "Venta de mango Temix 405 + 33", "efectivo": true},
 {"fecha": "May", "salida": "10000", "entrada": "0", "concepto": "Casita: mano de obra semana 3", "efectivo": true},
