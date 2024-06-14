27ae42569d9fb7f9cb13d7a882529489f8cc36db
ReyMon <ramon@reymon.mx>
2024-06-14 23:52:45 +0000

    inversion meliponas

--- site/static/data/melip.js
+++ site/static/data/melip.js
@@ -1,5 +1,7 @@
 var datosmeli=[
-{"monto": "22500", "concepto": "50 Cajas", "efectivo": true},
-{"monto": "8000", "concepto": "100 Trampas", "efectivo": true},
-{"monto": "9050", "concepto": "Meliponario", "efectivo": true},
+{"entrada": "0", "salida": "22500", "concepto": "50 Cajas", "efectivo": false},
+{"entrada": "0", "salida": "8000", "concepto": "100 Trampas", "efectivo": false},
+{"entrada": "0", "salida": "9050", "concepto": "Meliponario", "efectivo": false},
+{"entrada": "9050", "salida": "0", "concepto": "RRC", "efectivo": false},
+{"entrada": "30500", "salida": "0", "concepto": "Fondika", "efectivo": false},
 ];
