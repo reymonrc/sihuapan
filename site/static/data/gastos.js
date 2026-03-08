cb7ff29f2a3ddb2d1fb5ce734bea9ef2cb1f8b94
reymonrc <ramon@reymon.mx>
2026-03-08 15:21:49 -0600

    Actualizacion

--- site/static/data/gastos.js
+++ site/static/data/gastos.js
@@ -1,76 +1,79 @@
-var datos22=[
-{"fecha": "Jun", "salida": "0", "entrada": "48000", "concepto": "Venta mango", "efectivo": true},
-{"fecha": "Jul-Dic", "salida": "0", "entrada": "8000", "concepto": "Renta soltadero", "efectivo": true},
-{"fecha": "Jun", "salida": "0", "entrada": "15000", "concepto": "Renta 1ha tabaco-soltadero", "efectivo": true},
-{"fecha": "Jun", "salida": "0", "entrada": "15000", "concepto": "Renta 1ha tabaco-cocal", "efectivo": true},
-{"fecha": "Sep", "salida": "0", "entrada": "11000", "concepto": "Venta de leña, poda finca", "efectivo": true},
-{"fecha": "Nov", "salida": "0", "entrada": "4000", "concepto": "Renta 2has maiz-soltadero", "efectivo": true},
-{"fecha": "May-Dic", "salida": "77400", "entrada": "0", "concepto": "Mano de obra - campo", "efectivo": true},
-{"fecha": "Abr-Dic", "salida": "38000", "entrada": "0", "concepto": "Insumos - campo", "efectivo": true},
-{"fecha": "Dic", "salida": "0", "entrada": "5000", "concepto": "Aportación Arturo Carrión", "efectivo": false}
+var datos22 = [
+    { fecha: "Jun", salida: "0", entrada: "48000", concepto: "Venta mango", efectivo: true },
+    { fecha: "Jul-Dic", salida: "0", entrada: "8000", concepto: "Renta soltadero", efectivo: true },
+    { fecha: "Jun", salida: "0", entrada: "15000", concepto: "Renta 1ha tabaco-soltadero", efectivo: true },
+    { fecha: "Jun", salida: "0", entrada: "15000", concepto: "Renta 1ha tabaco-cocal", efectivo: true },
+    { fecha: "Sep", salida: "0", entrada: "11000", concepto: "Venta de leña, poda finca", efectivo: true },
+    { fecha: "Nov", salida: "0", entrada: "4000", concepto: "Renta 2has maiz-soltadero", efectivo: true },
+    { fecha: "May-Dic", salida: "77400", entrada: "0", concepto: "Mano de obra - campo", efectivo: true },
+    { fecha: "Abr-Dic", salida: "38000", entrada: "0", concepto: "Insumos - campo", efectivo: true },
+    { fecha: "Dic", salida: "0", entrada: "5000", concepto: "Aportación Arturo Carrión", efectivo: false }
 ];
 
-var datos23=[
-{"fecha": "Jun", "salida": "0", "entrada": "32000", "concepto": "Venta mango", "efectivo": true},
-{"fecha": "Ene-Dic", "salida": "0", "entrada": "34000", "concepto": "Renta soltadero", "efectivo": true},
-{"fecha": "Ene-Dic", "salida": "20000", "entrada": "0", "concepto": "Renta soltadero (debe)", "efectivo": true},
-{"fecha": "Jun", "salida": "0", "entrada": "37500", "concepto": "Renta 2.5has tabaco-soltadero", "efectivo": true},
-{"fecha": "Jun", "salida": "0", "entrada": "15000", "concepto": "Renta 1ha tabaco-cocal", "efectivo": true},
-{"fecha": "Nov", "salida": "0", "entrada": "4000", "concepto": "Renta 2has maiz-soltadero", "efectivo": true},
-{"fecha": "Ene-Dic", "salida": "75600", "entrada": "0", "concepto": "Mano de obra - campo", "efectivo": true},
-{"fecha": "Ene-Dic", "salida": "49000", "entrada": "0", "concepto": "Insumos - campo", "efectivo": true},
-{"fecha": "Dic", "salida": "0", "entrada": "5000", "concepto": "Aportación Arturo Carrión", "efectivo": false}
+var datos23 = [
+    { fecha: "Jun", salida: "0", entrada: "32000", concepto: "Venta mango", efectivo: true },
+    { fecha: "Ene-Dic", salida: "0", entrada: "34000", concepto: "Renta soltadero", efectivo: true },
+    { fecha: "Ene-Dic", salida: "20000", entrada: "0", concepto: "Renta soltadero (debe)", efectivo: true },
+    { fecha: "Jun", salida: "0", entrada: "37500", concepto: "Renta 2.5has tabaco-soltadero", efectivo: true },
+    { fecha: "Jun", salida: "0", entrada: "15000", concepto: "Renta 1ha tabaco-cocal", efectivo: true },
+    { fecha: "Nov", salida: "0", entrada: "4000", concepto: "Renta 2has maiz-soltadero", efectivo: true },
+    { fecha: "Ene-Dic", salida: "75600", entrada: "0", concepto: "Mano de obra - campo", efectivo: true },
+    { fecha: "Ene-Dic", salida: "49000", entrada: "0", concepto: "Insumos - campo", efectivo: true },
+    { fecha: "Dic", salida: "0", entrada: "5000", concepto: "Aportación Arturo Carrión", efectivo: false }
 ];
 
-var datos24=[
-{"fecha": "Ene", "salida": "0", "entrada": "1000", "concepto": "Renta 1ha sandia-cocal", "efectivo": true},
-{"fecha": "Mar-Jun", "salida": "0", "entrada": "10000", "concepto": "Renta soltadero", "efectivo": true},
-{"fecha": "Abr", "salida": "0", "entrada": "5000", "concepto": "Aportación Arturo Carrión", "efectivo": false},
-{"fecha": "Abr", "salida": "11940", "entrada": "0", "concepto": "Casita: block, cemento, cal", "efectivo": false},
-{"fecha": "Abr 27", "salida": "0", "entrada": "15330", "concepto": "Venta de mango Temix 405 + 33", "efectivo": true},
-{"fecha": "May", "salida": "10000", "entrada": "0", "concepto": "Casita: mano de obra semana 3", "efectivo": true},
-{"fecha": "May", "salida": "0", "entrada": "15000", "concepto": "Aportación Arturo Carrión", "efectivo": false},
-{"fecha": "May", "salida": "3200", "entrada": "0", "concepto": "Casita: piedra, arena", "efectivo": false},
-{"fecha": "May", "salida": "10000", "entrada": "0", "concepto": "Casita: mano de obra semana 1", "efectivo": true},
-{"fecha": "May", "salida": "2221", "entrada": "0", "concepto": "Casita: camento, cal, clavo, alambre", "efectivo": false},
-{"fecha": "May", "salida": "10000", "entrada": "0", "concepto": "Casita: mano de obra semana 2", "efectivo": true},
-{"fecha": "May", "salida": "0", "entrada": "5000", "concepto": "Aportación Esperanza casita", "efectivo": true},
-{"fecha": "May 19", "salida": "0", "entrada": "9350", "concepto": "Venta de mango Santos T 138 + 236 x $25", "efectivo": true},
-{"fecha": "May 26", "salida": "0", "entrada": "12000", "concepto": "Venta de mango Santos T 630", "efectivo": true},
-{"fecha": "May 26", "salida": "0", "entrada": "1750", "concepto": "Venta de mango Temix 70", "efectivo": true},
-{"fecha": "May", "salida": "10000", "entrada": "0", "concepto": "Casita: mano de obra semana 3", "efectivo": true},
-{"fecha": "Jun", "salida": "0", "entrada": "3375", "concepto": "Aportación Esperanza firme", "efectivo": false},
-{"fecha": "Jun", "salida": "3375", "entrada": "0", "concepto": "cemento firme 15 bultos", "efectivo": false},
-{"fecha": "Mayo-Ago", "salida": "23400", "entrada": "0", "concepto": "Mano de obra - campo", "efectivo": true},
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "", "efectivo": true},
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "", "efectivo": true},
-{"fecha": "Sep-Dic", "salida": "23400", "entrada": "0", "concepto": "Mano de obra - campo", "efectivo": true},
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "", "efectivo": true},
-{"fecha": "dic", "salida": "0", "entrada": "0", "concepto": "liquidacion trabajador", "efectivo": true},
+var datos24 = [
+    { fecha: "Ene", salida: "0", entrada: "1000", concepto: "Renta 1ha sandia-cocal", efectivo: true },
+    { fecha: "Mar-Jun", salida: "0", entrada: "10000", concepto: "Renta soltadero", efectivo: true },
+    { fecha: "Abr", salida: "0", entrada: "5000", concepto: "Aportación Arturo Carrión", efectivo: false },
+    { fecha: "Tomas de agua potable", salida: "0", entrada: "0", concepto: "160", efectivo: true },
+    { fecha: "Abr", salida: "11940", entrada: "0", concepto: "Casita: block, cemento, cal", efectivo: false },
+    { fecha: "Abr 27", salida: "0", entrada: "15330", concepto: "Venta de mango Temix 405 + 33", efectivo: true },
+    { fecha: "May", salida: "10000", entrada: "0", concepto: "Casita: mano de obra semana 3", efectivo: true },
+    { fecha: "May", salida: "0", entrada: "15000", concepto: "Aportación Arturo Carrión", efectivo: false },
+    { fecha: "May", salida: "3200", entrada: "0", concepto: "Casita: piedra, arena", efectivo: false },
+    { fecha: "May", salida: "10000", entrada: "0", concepto: "Casita: mano de obra semana 1", efectivo: true },
+    { fecha: "May", salida: "2221", entrada: "0", concepto: "Casita: camento, cal, clavo, alambre", efectivo: false },
+    { fecha: "May", salida: "10000", entrada: "0", concepto: "Casita: mano de obra semana 2", efectivo: true },
+    { fecha: "May", salida: "0", entrada: "5000", concepto: "Aportación Esperanza casita", efectivo: true },
+    { fecha: "May 19", salida: "0", entrada: "9350", concepto: "Venta de mango Santos T 138 + 236 x $25", efectivo: true},
+    { fecha: "May 26", salida: "0", entrada: "12000", concepto: "Venta de mango Santos T 630", efectivo: true },
+    { fecha: "May 26", salida: "0", entrada: "1750", concepto: "Venta de mango Temix 70", efectivo: true },
+    { fecha: "May", salida: "10000", entrada: "0", concepto: "Casita: mano de obra semana 3", efectivo: true },
+    { fecha: "Jun", salida: "0", entrada: "3375", concepto: "Aportación Esperanza firme", efectivo: false },
+    { fecha: "Jun", salida: "3375", entrada: "0", concepto: "cemento firme 15 bultos", efectivo: false },
+    { fecha: "Mayo-Ago", salida: "23400", entrada: "0", concepto: "Mano de obra - campo", efectivo: true },
+    { fecha: "Ago", salida: "2370", entrada: "0", concepto: "Insumos - campo", efectivo: true },
+    { fecha: "", salida: "0", entrada: "0", concepto: "", efectivo: true },
+    { fecha: "Ene-Dic", salida: "0", entrada: "100000", concepto: "Renta Soltadero", efectivo: true },
+    { fecha: "Nov", salida: "3400", entrada: "0", concepto: "", efectivo: true },
+    { fecha: "Dic", salida: "5200", entrada: "0", concepto: "liquidacion trabajador", efectivo: true }
 ];
 
-var datos25=[
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "Prediales 2025", "efectivo": true},
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "Dos tomas de agua potable", "efectivo": true},
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "lindero a los carriles - material", "efectivo": true},
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "lindero a los carriles - material", "efectivo": true},
-{"fecha": "Jun", "salida": "0", "entrada": "40000", "concepto": "Renta 2has tabaco-soltadero", "efectivo": true},
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "", "efectivo": true},
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "", "efectivo": true},
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "", "efectivo": true},
-{"fecha": "Ago", "salida": "0", "entrada": "2000", "concepto": "Renta 2has tabaco-soltadero", "efectivo": true},
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "", "efectivo": true},
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "", "efectivo": true},
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "", "efectivo": true},
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "", "efectivo": true},
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "", "efectivo": true},
-    ];
+var datos25 = [
+    { fecha: "Ene", salida: "0", entrada: "0", concepto: "Prediales 2025", efectivo: true },
+    { fecha: "Ene", salida: "60", entrada: "0", concepto: "Dos tomas de agua potable", efectivo: true },
+    { fecha: "Ene", salida: "0", entrada: "0", concepto: "lindero a los carriles - mano de obra", efectivo: true },
+    { fecha: "Ene", salida: "0", entrada: "0", concepto: "lindero a los carriles - material", efectivo: true },
+    { fecha: "May", salida: "0", entrada: "0", concepto: "perimetro finca - mano de obra", efectivo: true },
+    { fecha: "May", salida: "0", entrada: "0", concepto: "perimetro finca - mano de obra", efectivo: true },
+    { fecha: "May", salida: "0", entrada: "32000", concepto: "Cosecha de mango - Temix", efectivo: true },
+    { fecha: "Jun", salida: "0", entrada: "40000", concepto: "Renta 2has tabaco-soltadero", efectivo: true },
+    { fecha: "", salida: "0", entrada: "0", concepto: "", efectivo: true },
+    { fecha: "", salida: "0", entrada: "0", concepto: "", efectivo: true },
+    { fecha: "", salida: "0", entrada: "0", concepto: "", efectivo: true },
+    { fecha: "Ago", salida: "0", entrada: "2000", concepto: "Renta 1ha maiz - soltadero", efectivo: true },
+    { fecha: "", salida: "0", entrada: "0", concepto: "", efectivo: true },
+    { fecha: "", salida: "0", entrada: "0", concepto: "", efectivo: true },
+    { fecha: "", salida: "0", entrada: "0", concepto: "", efectivo: true },
+    { fecha: "", salida: "0", entrada: "0", concepto: "", efectivo: true },
+    { fecha: "", salida: "0", entrada: "0", concepto: "", efectivo: true }
+];
 
-var datos26=[
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "", "efectivo": true},
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "", "efectivo": true},
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "", "efectivo": true},
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "", "efectivo": true},
-{"fecha": "", "salida": "0", "entrada": "0", "concepto": "", "efectivo": true},
-    ];
-
+var datos26 = [
+    { fecha: "", salida: "0", entrada: "0", concepto: "", efectivo: true },
+    { fecha: "", salida: "0", entrada: "0", concepto: "", efectivo: true },
+    { fecha: "", salida: "0", entrada: "0", concepto: "", efectivo: true },
+    { fecha: "", salida: "0", entrada: "0", concepto: "", efectivo: true },
+    { fecha: "", salida: "0", entrada: "0", concepto: "", efectivo: true }
+];
